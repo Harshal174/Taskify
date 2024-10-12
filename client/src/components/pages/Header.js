c425680179c99933c68f00d06c20db4189ce5148
Harshal174 <kumarharshal2001@gmail.com>
2024-10-12 21:28:04 +0530

    minor tweakings

--- client/src/components/pages/Header.js
+++ client/src/components/pages/Header.js
@@ -1,40 +1,52 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { Box, Heading, Flex, Spacer, IconButton } from '@chakra-ui/react';
-import { FaUserCircle } from 'react-icons/fa'; // Importing the user icon
+import { FaUserCircle } from 'react-icons/fa'; 
 import { RxHamburgerMenu } from "react-icons/rx";
-import { Link,useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
 function Header() {
-  const location=useLocation();
+  const location = useLocation();
+  const [showProfileButton, setShowProfileButton] = useState(true);
+
+  useEffect(() => {
+    if (location.pathname === "/signup" || location.pathname === "/") {
+      setShowProfileButton(false);
+    } else {
+      setShowProfileButton(true);
+    }
+  }, [location.pathname]);
+
   const handleProfileClick = () => {
-    // Functionality to be added later
     console.log("Profile button clicked");
   };
 
-  const isListManagerPage=location.pathname==="/listmanager";
+  const isListManagerPage = location.pathname === "/listmanager";
+
   return (
     <Box bg="teal.500" color="white" p={4}>
       <Flex alignItems="center" position="sticky" width="100%" >
-     {isListManagerPage&&(
-      <IconButton
-          icon={<RxHamburgerMenu/>}
-          aria-label="Open Menu"
-          colorScheme="teal"
-          _hover={{ bg: "teal.600" }}
-          marginRight={"14px"}
-        />
-     )}
-        <Heading size="lg">
-        TASKIFY</Heading>
+        {isListManagerPage && (
+          <IconButton
+            icon={<RxHamburgerMenu />}
+            aria-label="Open Menu"
+            colorScheme="teal"
+            _hover={{ bg: "teal.600" }}
+            marginRight={"14px"}
+          />
+        )}
+        <Heading size="lg">TASKIFY</Heading>
         <Spacer />
-        <Link to="/Profile">
-        <IconButton
-          icon={<FaUserCircle />} // Using the user icon
-          aria-label="User Profile"
-          variant="outline"
-          colorScheme="whiteAlpha"
-          onClick={handleProfileClick} // Adding click handler
-        />
-        </Link>
+        {showProfileButton && (
+          <Link to="/Profile">
+            <IconButton
+              icon={<FaUserCircle />} 
+              aria-label="User Profile"
+              variant="outline"
+              colorScheme="whiteAlpha"
+              onClick={handleProfileClick} 
+            />
+          </Link>
+        )}
       </Flex>
     </Box>
   );
